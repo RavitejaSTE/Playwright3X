80e6b2a40f52990b547fa1db139c47ea122e608e
Raviteja BJ <ravitejabj@MacBook-Air-2.local>
2026-08-07 21:27:41 +0530

    function and string code push

---
+++ AssignmentOrHomework/TTA_CodingPractice/LeapYearChecker.js
@@ -0,0 +1,11 @@
+function leapYear(year){
+
+    if((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)){
+            console.log(`${year} is a Leap Year.`);
+    }else{
+            console.log(`${year} is not a Leap Year`);
+    }
+
+}
+
+leapYear(2024);
